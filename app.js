e5973375f73570200d941abfd5ccf6e466d6c2af
arief0303 <arief0303@gmail.com>
2022-06-20 17:23:54 +0700

    v1

--- app.js
+++ app.js
@@ -1,30 +1,56 @@
-import * as THREE from 'https://cdn.skypack.dev/three';
+import * as THREE from './build/three.module.js';
+import Stats from './jsm/libs/stats.module.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+import { GUI } from './jsm/libs/dat.gui.module.js';
+import { OrbitControls } from './jsm/controls/OrbitControls.js';
+import { MarchingCubes } from './jsm/objects/MarchingCubes.js';
+import { ToonShader1, ToonShader2, ToonShaderHatching, ToonShaderDotted } from './jsm/shaders/ToonShader.js';
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+var container, stats;
+
+var camera, scene, renderer;
+
+var materials, current_material;
+
+var light, pointLight, ambientLight;
+
+var pointLight2
+
+var effect, resolution;
+
+var effectController;
+
+var time = 0;
+var clock = new THREE.Clock();
+
+// animate();
+
+scene = new THREE.Scene();
+scene.background = null;
+camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 2000);
+
+renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
 camera.position.z = 5;
 
-function animate() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  cube.rotation.z += 0.01;
+function animateOnScroll() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    cube.rotation.z += 0.01;
 
-  renderer.render( scene, camera );
+    renderer.render(scene, camera);
 }
 
-document.body.onscroll = () => { 
-  animate()
-  console.log(document.body.offsetTop)
+document.body.onscroll = () => {
+    animateOnScroll()
+    console.log(document.body.offsetTop)
 }
 
 var text2 = document.createElement('div');
@@ -37,3 +63,11 @@ text2.innerHTML = "hi there!";
 text2.style.top = 200 + 'px';
 text2.style.left = 200 + 'px';
 document.body.appendChild(text2);
+
+function animate() {
+
+    requestAnimationFrame( animate );
+
+    render();
+    //stats.update();
+}
